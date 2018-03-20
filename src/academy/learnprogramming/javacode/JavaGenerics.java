72dc2335d5151a755040c4f3dfd6fd1159e3a6cb
Robert Lowe <robertmlowe@rmlowe.com>
2018-03-20 13:56:37 +0800

    Generics in Kotlin

--- src/academy/learnprogramming/javacode/JavaGenerics.java
+++ src/academy/learnprogramming/javacode/JavaGenerics.java
@@ -1,9 +1,18 @@
 package academy.learnprogramming.javacode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class JavaGenerics {
 
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Hello");
+        //list.add(new BigDecimal(10.5));
+        list.get(0).toUpperCase();
 
+        List list1 = new ArrayList<>();
+        list.add("Goodbye");
     }
-    
+
 }
